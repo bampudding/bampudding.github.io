5de3d97706f67641b4c5d9da77c8c2c23b2683aa
heart <66947260+bampudding@users.noreply.github.com>
2023-02-08 04:35:12 +0900

    g

--- asset/js/import.js
+++ asset/js/import.js
@@ -14,3 +14,12 @@ window.addEventListener('load', function() {
         }
     });
 });
+
+function setCookie(key, value, expiredays) {
+    let todayDate = new Date();
+    todayDate.setDate(todayDate.getDate() + expiredays); // 현재 시각 + 일 단위로 쿠키 만료 날짜 변경
+    //todayDate.setTime(todayDate.getTime() + (expiredays * 24 * 60 * 60 * 1000)); // 밀리세컨드 단위로 쿠키 만료 날짜 변경
+    document.cookie = key + "=" + escape(value) + "; path=/; expires=" + todayDate.toGMTString() + ";";
+}
+
+setCookie();
