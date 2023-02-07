5156619651f1ddd25abf7eba50b5604ea1e6e0ee
heart <66947260+bampudding@users.noreply.github.com>
2023-02-07 17:28:17 +0900

    d

--- asset/js/header-scroll.js
+++ asset/js/header-scroll.js
@@ -1,12 +1,12 @@
 window.addEventListener('scroll', function(e){
     let scroll = document.body.scrollTop;
-    if (scroll > 48) {
+    if (scroll > 24) {
         document.querySelector('.PC-Header').classList.add('Fix');
-        document.querySelector('.PC-Header_dummy').classList.remove('display-none');
+        document.querySelector('.PC-Header_dummy').classList.add('on');
     }
 
     else {
         document.querySelector('.PC-Header').classList.remove('Fix');
-        document.querySelector('.PC-Header_dummy').classList.add('display-none');
+        document.querySelector('.PC-Header_dummy').classList.remove('on');
     } 
 });
