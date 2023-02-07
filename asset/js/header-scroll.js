4cab1539e9695c4169d834112e8d710345bb81ce
heart <66947260+bampudding@users.noreply.github.com>
2023-02-07 14:46:10 +0900

    d

--- asset/js/header-scroll.js
+++ asset/js/header-scroll.js
@@ -1,16 +1,13 @@
-window.onload = () => {
+window.addEventListener('scroll', function(e){
+    let scroll = document.body.scrollTop
+    console.log(scroll)
+    if (scroll > 48) {
+        document.querySelector('.PC-Header').classList.add('Fix');
+        document.querySelector('.PC-Header_dummy').classList.remove('display-none');
+    }
 
-    window.addEventListener('scroll', function(){
-        let scroll = document.body.scrollTop
-        if (scroll > 48) {
-            document.querySelector('.PC-Header').classList.add('Fix');
-            document.querySelector('.PC-Header_dummy').classList.remove('display-none');
-        }
-
-        else {
-            document.querySelector('.PC-Header').classList.remove('Fix');
-            document.querySelector('.PC-Header_dummy').classList.add('display-none');
-        } 
-    });
-
-};
+    else {
+        document.querySelector('.PC-Header').classList.remove('Fix');
+        document.querySelector('.PC-Header_dummy').classList.add('display-none');
+    } 
+});
