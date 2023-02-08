881e3669b849fb277697867dc8b997132dee3c68
heart <66947260+bampudding@users.noreply.github.com>
2023-02-08 12:21:26 +0900

    d

--- asset/js/search_ui.js
+++ asset/js/search_ui.js
@@ -9,6 +9,17 @@ document.querySelector(".mobile-search").addEventListener("click", function(e){
     }
 });
 
+document.querySelector(".TitleSearch").addEventListener("click", function(e){
+    if ( document.querySelector('.search-wrap').classList.contains('on') ){
+        document.querySelector('.search-wrap').classList.remove('on');
+    }
+
+    else {
+        document.querySelector('.search-wrap').classList.add('on');
+        document.querySelector('.div-search input').focus();
+    }
+});
+
 document.querySelector(".sidebar-search").addEventListener("click", function(e){
     if ( document.querySelector('.search-wrap').classList.contains('on') ){
         document.querySelector('.search-wrap').classList.remove('on');
